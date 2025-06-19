adfbaddb5de7af8ddb78fd7ddb7493c1d48d5d59
stepandenisov <stdnsv@gmail.com>
2025-06-19 23:08:06 +0400

    initial commit

---
+++ src/main/java/ru/yandex/intershop/IntershopApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.intershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntershopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntershopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yandex/intershop/IntershopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.intershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntershopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
