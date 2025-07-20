c9e279fa1d80cf77c5f169ed6e7fa9e4e4e70773
stepandenisov <stdnsv@gmail.com>
2025-07-20 18:04:04 +0400

    image: redis caching

--- intershop-core/src/main/java/ru/yandex/intershop/service/ImageService.java
+++ intershop-core/src/main/java/ru/yandex/intershop/service/ImageService.java
@@ -1,23 +1,34 @@
 package ru.yandex.intershop.service;
 
 
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import ru.yandex.intershop.model.image.Image;
 import ru.yandex.intershop.repository.ImageRepository;
+
+import java.time.Duration;
 
 
 @Service
 public class ImageService {
 
     private final ImageRepository imageRepository;
+    private final ReactiveRedisTemplate<String, Image> imageReactiveRedisTemplate;
 
-    public ImageService(ImageRepository imageRepository){
+    private static final String IMAGE_CACHE_KEY = "image:";
+
+    public ImageService(ImageRepository imageRepository,
+                        ReactiveRedisTemplate<String, Image> imageReactiveRedisTemplate){
         this.imageRepository = imageRepository;
+        this.imageReactiveRedisTemplate = imageReactiveRedisTemplate;
     }
 
     public Mono<Image> getImageByItemId(Long itemId){
-        return imageRepository.findImageByItemId(itemId);
+        return imageReactiveRedisTemplate.opsForValue().get(IMAGE_CACHE_KEY+itemId)
+                .switchIfEmpty(imageRepository.findImageByItemId(itemId)
+                        .flatMap(image -> imageReactiveRedisTemplate.opsForValue().set(IMAGE_CACHE_KEY+itemId, image, Duration.ofMinutes(1))
+                                .then(Mono.just(image))));
     }
 
     public Mono<Image> save(Image image){
